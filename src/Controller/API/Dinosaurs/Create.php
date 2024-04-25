f6737c6841032248087bb87259ff3f13a650535d
AntoineGonzalez <45924026+AntoineGonzalez@users.noreply.github.com>
2024-04-25 10:25:42 +0200

    Practice 4: JSON Schema Validation

--- src/Controller/API/Dinosaurs/Create.php
+++ src/Controller/API/Dinosaurs/Create.php
@@ -8,21 +8,42 @@ use App\Entity\Dinosaur;
 use App\Entity\Species;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Opis\JsonSchema\Validator as JsonSchemaValidator;
+use Opis\JsonSchema\Errors\ErrorFormatter;
 
 final class Create extends AbstractController
 {
+    public function __construct(
+        #[Autowire('%kernel.project_dir%/jsonSchema')]
+        private readonly string $jsonSchemaDir
+    ) {
+    }
+
     #[Route('/api/dinosaurs', methods: 'POST')]
     public function __invoke(
         ManagerRegistry $manager,
         Request $request,
         SerializerInterface $serializer
     ): Response {
-        $dinosaurData = json_decode($request->getContent(), true);
+        $dinosaurData = json_decode($request->getContent());
+
+        $validator = new JsonSchemaValidator();
+        $validator->resolver()->registerPrefix('http://localhost/api', $this->jsonSchemaDir);
+
+        $result = $validator->validate($dinosaurData, 'http://localhost/api/dinosaur/create.json');
+
+        if ($result->isValid() === false) {
+            return new JsonResponse([
+                'message' => 'Dinosaur creation failed',
+                'errors' => (new ErrorFormatter())->format($result->error())
+            ], Response::HTTP_BAD_REQUEST);
+        }
 
         $species = $manager
             ->getRepository(Species::class)
@@ -34,6 +55,8 @@ final class Create extends AbstractController
                 Response::HTTP_NOT_FOUND
             ]);
         }
+
+        $dinosaurData = json_decode($request->getContent(), true);
 
         try {
             $dinosaur = new Dinosaur(
