d0bc56aa2ba8cf8059e8aa9a9c77f1d1116df193
AntoineGonzalez <45924026+AntoineGonzalez@users.noreply.github.com>
2024-05-29 10:56:10 +0200

    Practice 3: Manage JavaScript & CSS with AssetMapper

---
+++ importmap.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'app' => [
+        'path' => './assets/app.js',
+        'entrypoint' => true,
+    ],
+    'bootstrap' => [
+        'version' => '5.3.3',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.3',
+        'type' => 'css',
+    ],
+    'home' => [
+        'path' => './assets/home.js',
+        'entrypoint' => true,
+    ],
+    'habitat' => [
+        'path' => './assets/habitat.js',
+        'entrypoint' => true,
+    ],
+    'dinosaurs' => [
+        'path' => './assets/dinosaurs.js',
+        'entrypoint' => true,
+    ],
+];
