d143015c979973a135d56fbed8baa0026b48bfc0
AntoineGonzalez <45924026+AntoineGonzalez@users.noreply.github.com>
2024-05-30 16:27:24 +0200

    Practice 8: Twig components - content block and props validation

--- src/Twig/Components/Atoms/NavLink.php
+++ src/Twig/Components/Atoms/NavLink.php
@@ -4,14 +4,30 @@ declare(strict_types=1);
 
 namespace App\Twig\Components\Atoms;
 
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Symfony\UX\TwigComponent\Attribute\PreMount;
 
 #[AsTwigComponent()]
 final class NavLink
 {
-    public string $title;
     public string $href;
-    public ?bool $isActive = false;
+    public ?bool $isActive;
+
+    #[PreMount]
+    public function preMount(array $data): array
+    {
+        $resolver = new OptionsResolver();
+
+        $resolver->define('isActive');
+        $resolver->define('href');
+        $resolver->setAllowedTypes('isActive', 'bool');
+        $resolver->setAllowedTypes('href', 'string');
+        $resolver->setRequired('href');
+        $resolver->setDefault('isActive', false);
+
+        return $resolver->resolve($data);
+    }
 
     public function getClass(): string
     {
--- src/Twig/Components/Molecules/NavItem.php
+++ src/Twig/Components/Molecules/NavItem.php
@@ -5,19 +5,32 @@ declare(strict_types=1);
 namespace App\Twig\Components\Molecules;
 
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Symfony\UX\TwigComponent\Attribute\PreMount;
 
 #[AsTwigComponent]
 final class NavItem
 {
-    public string $title;
     public string $route;
 
     public function __construct(
         private readonly RouterInterface $router,
         private readonly RequestStack $requestStack,
     ) {
+    }
+
+    #[PreMount]
+    public function preMount(array $data): array
+    {
+        $resolver = new OptionsResolver();
+
+        $resolver->define('route');
+        $resolver->setAllowedTypes('route', 'string');
+        $resolver->setRequired('route');
+
+        return $resolver->resolve($data);
     }
 
     public function isActive(): bool
